5a034cc2fbb592d58d555ba00d20ba44a23e7fa4
houxinlin <houxinlin@seczone.cn>
2023-12-19 16:24:10 +0800

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,13 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("com.google.code.gson:gson:2.10.1")
 
 }
 
