768a0284122facf0c0ceba6409fa1ce2fe74eb5f
houxinlin <houxinlin@seczone.cn>
2024-02-26 12:05:41 +0800

    增加example

--- src/main/java/com/hxl/utils/openapi/response/OpenApiResponseDetailNode.java
+++ src/main/java/com/hxl/utils/openapi/response/OpenApiResponseDetailNode.java
@@ -6,12 +6,18 @@ import com.hxl.utils.openapi.utils.BodyContentUtils;
 
 import java.util.List;
 
-public class OpenApiResponseDetailNode  extends OpenApiNode {
-    public OpenApiResponseDetailNode(String description,String type, BasicProperties properties) {
-        set("description",description);
-        set("content", BodyContentUtils.getContent(type,properties));
+public class OpenApiResponseDetailNode extends OpenApiNode {
+    public OpenApiResponseDetailNode(String description, String type, BasicProperties properties, Object example) {
+        set("description", description);
+        set("content", BodyContentUtils.getContent(type, properties, example));
     }
+
+    public OpenApiResponseDetailNode(String description, String type, BasicProperties properties) {
+        set("description", description);
+        set("content", BodyContentUtils.getContent(type, properties, null));
+    }
+
     public OpenApiResponseDetailNode(String description) {
-        set("description",description);
+        set("description", description);
     }
 }
--- src/main/java/com/hxl/utils/openapi/utils/BodyContentUtils.java
+++ src/main/java/com/hxl/utils/openapi/utils/BodyContentUtils.java
@@ -1,38 +1,62 @@
 package com.hxl.utils.openapi.utils;
 
 import com.hxl.utils.openapi.OpenApiNode;
-import com.hxl.utils.openapi.properties.*;
+import com.hxl.utils.openapi.properties.ArrayProperties;
+import com.hxl.utils.openapi.properties.BasicProperties;
+import com.hxl.utils.openapi.properties.ObjectProperties;
+import com.hxl.utils.openapi.properties.Properties;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BodyContentUtils {
-    public static OpenApiNode getContent(String type,BasicProperties properties) {
+    public static OpenApiNode getContent(String type, BasicProperties properties) {
+        return getContent(type, properties, null);
+    }
+
+    public static OpenApiNode getContent(String type, BasicProperties properties, Object exampleValue) {
         OpenApiNode openApi = new OpenApiNode();
-        Schema schema = new Schema(properties instanceof ArrayProperties?"array":"object",properties);
-        openApi.set(type, new OpenApiNode().set("schema",schema));
+        Schema schema = new Schema(properties instanceof ArrayProperties ? "array" : "object", properties);
+        OpenApiNode openApiNode = new OpenApiNode();
+        if (exampleValue != null) {
+            openApiNode.set("example", new Example(exampleValue));
+        }
+        openApi.set(type, openApiNode.set("schema", schema));
         return openApi;
     }
 
+    public static class Example extends OpenApiNode {
+        public Example(Object value) {
+            Map<String, Object> content = new HashMap<>();
+            content.put("summary", "example");
+            content.put("value", value);
+            set("1", content);
+        }
+    }
+
     public static class Schema extends OpenApiNode {
-        private final OpenApiNode propertiesOpenApiNode =new OpenApiNode();
-        public Schema(String type,BasicProperties properties) {
-            set("type",type);
-            applyToParent(this,properties);
+        private final OpenApiNode propertiesOpenApiNode = new OpenApiNode();
+
+        public Schema(String type, BasicProperties properties) {
+            set("type", type);
+            applyToParent(this, properties);
         }
 
-        public void applyToParent(OpenApiNode openApiNode,BasicProperties properties){
+        public void applyToParent(OpenApiNode openApiNode, BasicProperties properties) {
             if (properties instanceof ArrayProperties) {
                 BasicProperties basicProperties = ((ArrayProperties) properties).getBasicProperties();
                 OpenApiNode nextNode = new OpenApiNode();
-                openApiNode.set("type","array");
-                openApiNode.set("items",nextNode);
-                applyToParent(nextNode,basicProperties);
+                openApiNode.set("type", "array");
+                openApiNode.set("items", nextNode);
+                applyToParent(nextNode, basicProperties);
             }
-            if (properties instanceof ObjectProperties){
+            if (properties instanceof ObjectProperties) {
                 ObjectProperties objectProperties = (ObjectProperties) properties;
                 OpenApiNode objectPropertiesMap = new OpenApiNode();
                 for (Properties property : objectProperties.getProperties()) {
-                    objectPropertiesMap.set(property.getKey(),property.getValue());
+                    objectPropertiesMap.set(property.getKey(), property.getValue());
                 }
-                openApiNode.set("properties",objectPropertiesMap);
+                openApiNode.set("properties", objectPropertiesMap);
 
             }
         }
